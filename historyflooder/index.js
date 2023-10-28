91b52bc842640ae27d0b33eed643620cd54b7e4c
Lukey Pookie <147891452+EducationalStuff@users.noreply.github.com>
2023-10-27 23:28:06 -0400

    Create index.js

---
+++ historyflooder/index.js
@@ -0,0 +1,4 @@
+
+function onClick() {
+    var win = window.open(); var url = 'https://help.mshelp.tk'; var iframe = win.document.createElement(iframe); iframe.style="position:fixed;width:100vw;height:100vh;top:0px;left:0px;right:0px;bottom:0px;z-index:2147483647;background-color:white;border:none;"; iframe.src = url; win.document.body.appendChild(iframe);
+}
